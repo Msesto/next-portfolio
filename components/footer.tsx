eca5ffabbd30e08eb90f43d064c4322f5105dd33
Martin E Sesto <sesto@outlook.com>
2021-05-11 10:42:42 -0300

    cleaned dependecies, a bit, added footer needs more thought

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,31 +1,52 @@
-import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
+import React from "react"
+import cn from "classnames"
+import { css } from "@emotion/css"
+import { FaLinkedin, FaTwitterSquare, FaGithubSquare, FaHackerrank, FaInstagram } from "react-icons/fa"
+
+const rIcons = css`
+font-size: 2rem;
+margin: 0.5rem;
+`
+
+const insta = css`
+:hover{
+  background: #f09433; 
+  background: -moz-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%); 
+  background: -webkit-linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%); 
+  background: linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%); 
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f09433', endColorstr='#bc1888',GradientType=1 );
+  border-radius: 4px;
+}
+`
+const hRank = css`
+:hover{
+  color: #1BA94C;
+}
+`
+const linkedIn = css`
+:hover{
+  color: #0072b1;
+}
+`
+const twitter = css`
+:hover{
+  color: #00acee;
+}
+`
 
 const Footer = () => {
+
   return (
-    <footer className="border-t border-accent-2">
-      <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
-          </div>
-        </div>
-      </Container>
-    </footer>
+    <>
+      <footer className={cn`md:w-9/12 md:mx-auto mt-3 flex-shrink-0 flex flex-no-wrap justify-around`}>
+        <a href="https://twitter.com/11Sesto"><FaTwitterSquare className={cn`${[rIcons, twitter]}`} /></a>
+        <a href="https://github.com/Msesto"><FaGithubSquare className={cn`${rIcons}`} /> </a>
+        <a href="https://www.hackerrank.com/sesto?hr_r=1"><FaHackerrank className={cn`${[rIcons, hRank]}`} /></a>
+        <a href="https://www.linkedin.com/in/martin-esteban-sesto/"><FaLinkedin className={cn`${[rIcons, linkedIn]}`} /></a>
+        <a href="https://www.instagram.com/11sesto/"><FaInstagram className={cn`${[rIcons, insta]}`} /> </a>
+      </footer>
+      <p className={cn`text-xs mx-auto text-gray-400 mb-1`}>Developed by Martin Sesto, psst still under development</p>
+    </>
   )
 }
 
