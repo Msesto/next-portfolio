aba28a44263cf9ba724d8d36e0c397e937142659
Martin E Sesto <sesto@outlook.com>
2021-05-11 19:43:03 -0300

    Added contact form, needs re-styling, made it in typescript (used Type Assertion for React.formEvent => HTMLInputElement)

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,58 +1,77 @@
+import React, { useState } from "react"
+import axios from "axios"
+import cn from "classnames"
+import Layout from "../components/layout"
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../types/post'
 
-type Props = {
-  allPosts: Post[]
+interface IStatus {
+  submitting: boolean;
+  status?: { ok: boolean; msg: string};
 }
 
-const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+const Contact = () => {
+
+  const [serverState, setServerState] = useState<IStatus>({
+    submitting: false,
+    status: undefined
+  })
+
+  const handleServerResponse = (ok:boolean, msg:string, form:HTMLFormElement) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg }
+    });
+    if (ok) {
+      form.reset();
+    }
+  }
+
+  const handleOnSubmit = (e:React.FormEvent) => {
+    e.preventDefault();
+    const form = e.target;
+    setServerState({ submitting: true })
+    axios({
+      method: "post",
+      url: "https://getform.io/f/4bae35e4-0284-4650-b24a-3e830d38f0ac",
+      data: new FormData(form as HTMLFormElement)
+    })
+      .then(r => {
+        handleServerResponse(true, "Thank, I will back to you soon!", form as HTMLFormElement )
+      })
+      .catch(r => {
+        handleServerResponse(false, r.response.data.error, form as HTMLFormElement )
+      })
+  }
+
   return (
-    <>
-      <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
+    <Layout>
+      <Container>
+        <section className={cn``}>
+          <h3 className={cn`mt-6 leading-tight text-3xl`}>Submit a contact form</h3>
+          <p className={cn`ml-4 leading-none font-light text-sm`}>If the form isn't working, email me at: <span className={cn`select-all cursor-pointer`}><em>sesto@outlook.com</em></span></p>
+          <form className={cn`items-center flex flex-col`} onSubmit={handleOnSubmit}>
+            <div className={cn`w-2/3 flex-shrink-0 mt-2`}>
+              <label className={cn`mt-2 md:mt-12 block`}>Email address:</label>
+              <input className={cn`md:mt-1 text-gray-700 p-2 rounded w-full`} type="email" name="email" placeholder="JuanDoe@example.com" required={true} />
+            </div>
+            <div className={cn` w-2/3 flex-shrink-0 mt-2`}>
+              <label className={cn`mt-2 md:mt-6 block`}>Name:</label>
+              <input className={cn`md:mt-1 text-gray-700 p-2 rounded w-full`} type="text" name="name" placeholder="Juan Doe" required={true} />
+            </div>
+            <div className={cn`w-2/3 flex-shrink-0 mt-2`}>
+              <label className={cn`mt-2 md:mt-6 block`}>Message:</label>
+              <textarea className={cn`md:mt-1 text-gray-700 mb-1 md:mb-4 h-24 p-2 rounded w-full`} name="message" placeholder="Enter your message"></textarea>
+            </div>
+            <button className={cn`p-1 w-1/3 text-3xl font-light text-black rounded-md bg-yellow-500 mt-2`} type="submit" disabled={serverState.submitting}>
+              Submit
+            </button>
+            {serverState.status && (<p className={!serverState.status.ok ? "errorMsg" : ""}>{serverState.status.msg}</p>)}
+          </form>
+        </section>
+      </Container>
+    </Layout>
+
   )
 }
 
-export default Index
-
-export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
-
-  return {
-    props: { allPosts },
-  }
-}
+export default Contact
