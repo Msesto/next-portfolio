921624dec426645bb657828fdf0498f5b040068b
Martin E Sesto <sesto@outlook.com>
2021-05-11 09:57:42 -0300

    Modified all favicons, changed the mobile logo (same as favicon), cleaned up code in header

--- components/header.tsx
+++ components/header.tsx
@@ -41,15 +41,10 @@ export default function Example() {
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
                   <img
-                    className="block md:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                    alt="Workflow"
+                    className="block md:hidden h-12 w-auto"
+                    src="/assets/blog/authors/ms.png"
+                    alt="Martin Sesto's Logo"
                   />
-                  {/* <img
-                    className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                    alt="Workflow"
-                  /> */}
                 <h2 className="hidden md:block text-3xl">MARTIN<strong>SESTO</strong></h2>
                 </div>
                 <div className="hidden sm:block sm:ml-6">
