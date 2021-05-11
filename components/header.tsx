ee8030a88640c1c106c26f69c31ef8f68f34a6e0
Martin E Sesto <sesto@outlook.com>
2021-05-11 09:07:43 -0300

    Redesigned the header, copied index to all the pages as placeholders for navbar

--- components/header.tsx
+++ components/header.tsx
@@ -1,94 +1,99 @@
-/** @jsx jsx */
-import { jsx } from "@emotion/react"
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from 'react'
+import { Disclosure } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
-import { slide as Menu } from "react-burger-menu"
-import cn from 'classnames'
-import { css } from "@emotion/css"
 
-const padMe = css`
-  padding : 0.5rem calc((100vw - 850px)/2);
-  margin-left: 10px;
-`
+const navigation = [
+  { name: 'About', href: '/', current: true },
+  { name: 'Projects', href: '/projects', current: false },
+  { name: 'Technologies', href: '/technologies', current: false },
+  { name: 'Resume', href: '/resume', current: false },
+  { name: 'Blog', href: '/blog', current: false },
+  { name: 'Contact', href: '/contact', current: false },
+]
 
-const styles = {
-  bmBurgerButton: {
-    position: 'fixed',
-    width: '36px',
-    height: '30px',
-    left: '36px',
-    top: '36px'
-  },
-  bmBurgerBars: {
-    background: '#373a47'
-  },
-  bmBurgerBarsHover: {
-    background: '#a90000'
-  },
-  bmCrossButton: {
-    height: '24px',
-    width: '24px'
-  },
-  bmCross: {
-    background: '#bdc3c7'
-  },
-  bmMenuWrap: {
-    position: 'fixed',
-    height: '100%'
-  },
-  bmMenu: {
-    background: '#373a47',
-    padding: '2.5em 1.5em 0',
-    fontSize: '1.15em'
-  },
-  bmMorphShape: {
-    fill: '#373a47'
-  },
-  bmItemList: {
-    color: '#b8b7ad',
-    padding: '0.8em'
-  },
-  bmItem: {
-    display: 'inline-block'
-  },
-  bmOverlay: {
-    background: 'rgba(0, 0, 0, 0.3)'
-  }
+function classNames(...classes:string[]) {
+  return classes.filter(Boolean).join(' ')
 }
 
-const linkMe = css`
-  color: #fff;
-  padding: 0.25rem;
-  margin: 0 0 0.5rem 0 0;
-
-  &.current-page{
-    border-bottom: 2px solid #ed8936;
-   }
-`
-
-const Header = () => {
+export default function Example() {
+  const router = useRouter()
+  
   return (
-    <header className={cn`flex p-2 justify-between ${padMe}`}>
-      <button className={cn`text-white text-4xl leading-tight`}><Link href="/"><p>MARTIN<strong>SESTO</strong></p></Link></button>
-      <nav className={cn`sm:hidden md:inline m-auto mx-0`}>
-        <button className={cn`m-1 text-base font-medium leading-tight ${linkMe}`}><Link href="/"> About </Link></button>
-        <button className={cn`m-1 text-base font-medium leading-tight ${linkMe}`}><Link href="/projects"> Projects </Link></button>
-        <button className={cn`m-1 text-base font-medium leading-tight ${linkMe}`}><Link href="/technologies"> Technologies </Link></button>
-        <button className={cn`m-1 text-base font-medium leading-tight ${linkMe}`}><Link href="/resume"> Resume </Link></button>
-        <button className={cn`m-1 text-base font-medium leading-tight ${linkMe}`}><Link href="/blog"> Blog </Link></button>
-        <button className={cn`m-1 text-base font-medium leading-tight ${linkMe}`}><Link href="/contact"> Contact </Link></button>
-      </nav>
-      {/* <div id="11" className={cn``}>
-        <Menu className={cn``} styles={styles} width={'150px'} isOpen={true}>
-          <button className={cn`m-1 text-base font-medium leading-tight ${linkMe}`}><Link href="/">About</Link></button>
-          <button className={cn`m-1 text-base font-medium leading-tight ${linkMe}`}><Link href="/projects">Projects</Link></button>
-          <button className={cn`m-1 text-base font-medium leading-tight ${linkMe}`}><Link href="/technologies">Technologies</Link></button>
-          <button className={cn`m-1 text-base font-medium leading-tight ${linkMe}`}><Link href="/resume">Resume</Link></button>
-          <button className={cn`m-1 text-base font-medium leading-tight ${linkMe}`}><Link href="/blog">Blog</Link></button>
-          <button className={cn`m-1 text-base font-medium leading-tight ${linkMe}`}><Link href="/contact">Contact</Link></button>
-        </Menu>
-      </div> */}
-    </header >
+    <Disclosure as="nav">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                  <img
+                    className="block md:hidden h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
+                  />
+                  {/* <img
+                    className="hidden lg:block h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    alt="Workflow"
+                  /> */}
+                <h2 className="hidden md:block text-3xl">MARTIN<strong>SESTO</strong></h2>
+                </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <Link href={item.href}>
+                        <a
+                          key={item.name}
+                          className={classNames(
+                            router.asPath === item.href ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            'px-3 py-2 rounded-md text-sm font-medium'
+                          )}
+                          aria-current={router.asPath === item.href ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <Link href={item.href}>
+                  <a
+                    key={item.name}
+                    className={classNames(
+                      router.asPath === item.href ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'block px-3 py-2 rounded-md text-base font-medium'
+                    )}
+                    aria-current={router.asPath === item.href ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </a>
+                </Link>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   )
 }
-
-export default Header
