bd0ace29fd2d916dff6567b9655eb888086b41fd
Martin E Sesto <sesto@outlook.com>
2021-05-18 17:45:11 -0300

    changed location to troubleshoot styles in production

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -7,9 +7,36 @@ import { NAME } from '../lib/constants'
 import cn from "classnames"
 import Tab from "../components/resume-tab"
 import { FaBook, FaBriefcase, FaKeyboard } from "react-icons/fa"
-import {IPart, EXP, SKILL, EDU} from "../lib/constants"
+import {IPart, SKILL, EDU} from "../lib/constants"
 
 const base = cn(`px-6 py-3 w-1/2 w-auto justify-center justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 tracking-wide md:tracking-widest text-lg md:text-xl rounded-t`)
+
+const EXP :IPart = {title: "Log of every professional job I had and a summary of each.", 
+  sections: [
+    ["Web Developer, Freelance.", `
+    <h4 class="text-2xl font-bold text-white text-opacity-60"> 2021 &rarr; </h4>
+    <ul class="list-disc p-1 pl-3 text-sm md:text-lg">
+      <li> Working with TS/React and Golang </li>
+      <li> Projects will be added to the projects page </li>
+      <li> I'd like to add that working on random jobs has given me more troubleshooting skills </li>
+    </ul>
+      `],
+    ["SUR Analyst, OSPLAD.", `
+    <h4 class="text-2xl font-bold text-white text-opacity-60"> 2018 - 2021 </h4>
+    <ul class="list-disc p-1 pl-3 text-sm md:text-lg">
+      <li> Administrative tasks, such as organizing all the billing for expensive medication, as well as auditing it, and making the files to send </li>
+      <li> During the pandemic, I was the only one from my team to keep going and did my best to keep the flow, until my resignation </li>
+      <li> Mainly got the gist of working with multiple teams of different types of professionals and be able to negotiate to reach a mutual ground </li>
+    </ul>
+    `], 
+    ["Tech Support, NewCar Group.", `
+    <h4 class="text-2xl font-bold text-white text-opacity-60"> 2017 - 2018 </h4>
+    <ul class="list-disc p-1 pl-3 text-sm md:text-lg">
+      <li> Maintained the work computers, printers and installed AP to reach places that didn't have wi-fi </li>
+      <li> Helped other employees to maximize their use of office software </li>
+    </ul>
+    `],
+]}
 
 const maperinho = (el:IPart) => {
   return(
