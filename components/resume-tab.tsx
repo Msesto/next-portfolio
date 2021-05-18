76385be980dffca608290c409fd82139c983eec2
Martin E Sesto <sesto@outlook.com>
2021-05-18 14:01:38 -0300

    Removed duplicate project/blogs (they were used as placeholder), changed style and data in resume page

--- components/resume-tab.tsx
+++ components/resume-tab.tsx
@@ -14,7 +14,7 @@ const Tab = ({
 }: Props) => {
   return (
     <section>
-      <h3 className='m-2 mb-6 text-2xl'>{title}</h3>
+      <h3 className='m-2 mb-6 text-lg md:text-2xl'>{title}</h3>
       { sections.map((e, i) => 
         <Drop e={e} i={i} />
       )}
@@ -32,7 +32,7 @@ const Drop = ({e, i}:DProps) => {
   return(
     <div className={ i%2 === 0 ? "bg-accent-1 rounded-lg" : ""}>
       <div className="flex justify-between p-2">
-        <h3 className="text-4xl">{e[0]}</h3>
+        <h3 className="text-2xl md:text-4xl">{e[0]}</h3>
         <a className="mt-2" onClick={() => setOpen(!useOpen)}>
           {useOpen ? 
           <FaTimes className="text-3xl text-yellow-500"/>
@@ -41,7 +41,7 @@ const Drop = ({e, i}:DProps) => {
           } 
         </a>
       </div>
-      {useOpen ? e[1] ? <div className="p-1 pl-6 tracking-tight leading-none text-lg" dangerouslySetInnerHTML={{__html: e[1]}} /> : null : null}
+      {useOpen ? e[1] ? <div className="p-1 pb-3 pl-6 tracking-tight leading-none text-lg" dangerouslySetInnerHTML={{__html: e[1]}} /> : null : null}
     </div>
 
   )
